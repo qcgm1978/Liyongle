bfa0ee672d71ab7723aec1d515bf79ec2d556b2d
张红亮 <zhanghongliang@zhanghoiangsAir.lan>
2019-07-28 22:13:33 +0800

    logarithm comparison

--- __test__/function.spec.js
+++ __test__/function.spec.js
@@ -1,3 +1,4 @@
+const math = require("mathjs");
 it(`有命题p、q，如果p推出q且q推出p，则p是q的充分必要条件，简称充要条件。
 p推出q，p是q的充分条件，同时q是p的必要条件，此时p是q的子集。`, () => {
   const p = [1, 2, 3],
@@ -9,4 +10,16 @@ p推出q，p是q的充分条件，同时q是p的必要条件，此时p是q的子
   p.forEach(element => {
     expect(q).toContain(element);
   });
+});
+it(`logarithm comparison`, () => {
+  expect(math.log(1 / 2, 3)).toBeLessThan(0);
+  //a=2^0.5, b=lg(PI,3), c=lg(2,sin(2/5*PI))
+  expect(math.pi).toBe(3.141592653589793);
+  const a = math.pow(2, 0.5),
+    b = math.log(3, math.pi),
+    c = math.log(2, math.sin((2 / 5) * math.pi));
+  expect(c)
+    .toBeLessThan(0)
+    .toBeLessThan(a)
+    .toBeLessThan(b);
 });
