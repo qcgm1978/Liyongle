41aa2a83a63a6456f2932fac34e11f757c572f2a
张红亮 <zhanghongliang@zhanghoiangsAir.lan>
2019-07-30 23:41:36 +0800

    elastic collision

--- __test__/physics.spec.js
+++ __test__/physics.spec.js
@@ -1,4 +1,5 @@
-it(` elastic collision`, () => {
+it(`elastic collision`, () => {
+  // exchange velocity, e.g. Newton's cradle
   let v = 1;
   const m1 = v,
     m2 = -v;
@@ -7,4 +8,15 @@ it(` elastic collision`, () => {
   expect(m1 + m2)
     .toBe(m3 + m4)
     .toBe(0);
+  // passing shot
+  let bigM = v,
+    smallM = v;
+  bigM = -v;
+  smallM = 2 * v - bigM;
+  expect(smallM)
+    .toBe(3 * v)
+    .toBe(3);
+  const distance = 1;
+  const reboundDistance = distance * smallM ** 2;
+  expect(reboundDistance).toBe(9);
 });
